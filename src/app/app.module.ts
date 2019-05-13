99f757fa2d7bf1b16cb0cfb63ff0879c0bf0a552
mrm-dev1 <mrmtechnologies@outlook.com>
2019-05-12 21:29:51 -0400

    angular 7 clean install with material desgin bootstrap 4
    angular 7 with angular md boodtrap 7 with functioning single component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// For MDB Angular Free
+import { MDBBootstrapModule} from 'angular-bootstrap-md';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
